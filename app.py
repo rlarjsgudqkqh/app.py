188582f06de0a9506008a84b04764a267b8a2a71
rlarjsgudqkqh <justinkim101221@gmail.com>
2026-07-20 14:58:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import time
 
+
 def reset_game():
     st.session_state.start_time = 0
     st.session_state.end_time = 0
-    st.session_state.result = 0
+    st.session_state.result = 0.0
 
-if 'start_time' not in st.session_state:
+
+if "start_time" not in st.session_state:
     reset_game()
 
 st.title("10초 맞추기 게임!")
@@ -18,6 +20,7 @@ with col1:
     if st.button("시작"):
         st.session_state.start_time = time.time()   # 현재 시간 기록
         st.session_state.end_time = 0               # 종료 시간 초기화
+        st.session_state.result = 0.0               # 결과 초기화
 
 with col2:
     if st.button("종료"):
@@ -26,7 +29,8 @@ with col2:
 
             # 걸린 시간 계산 (종료 시간 - 시작 시간)
             st.session_state.result = (
-                st.session_state.end_time - st.session_state.start_time
+                st.session_state.end_time
+                - st.session_state.start_time
             )
         else:
             st.warning("시작 버튼을 먼저 눌러주세요!")
@@ -34,12 +38,12 @@ with col2:
 if st.session_state.end_time != 0:
     diff = st.session_state.result
 
-    st.header(f"⏱결과: {diff:.2f}초")
+    st.header(f"⏱ 결과: {diff:.2f}초")
 
     # 성공 판정 (9.7초 ~ 10.3초 사이)
     if 9.7 <= diff <= 10.3:
         st.success("대단해요! 정확합니다!")
     else:
-        st.error(f"10초와 {abs(10-diff):.2f}초 차이가 납니다. 다시 도전해보세요!")
+        st.error(f"10초와 {abs(10 - diff):.2f}초 차이가 납니다. 다시 도전해보세요!")
 
 st.button("다시 하기", on_click=reset_game)
