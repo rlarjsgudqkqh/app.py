4babb72d74f08a37b141b5db9e781d7195fc3a6a
rlarjsgudqkqh <justinkim101221@gmail.com>
2026-07-20 13:39:31 +0900

    Use session state for count in Streamlit app

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 st.title("카운터 앱")
 
-count = 0
+if 'count' not in st.session_state:
+    st.session_state.count = 0
 
 if st.button("증가"):
-    count = count + 1
+    st.session_state.count += 1
 
-st.markdown(f"## 현재 숫자: `{count}`")
+st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
